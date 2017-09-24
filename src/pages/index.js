d24960c8e85f6a84e7092927c1d3b1fef8e24b95
Andric Tham <andric@nuisance.co>
2017-09-24 17:10:56 +0800

    Add link to a page that does not yet exist

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,11 @@
 import React from "react"
+import Link from "gatsby-link"
 
 export default () =>
   <div style={{color: `tomato`}}>
     <h1>Hello Gatsby!</h1>
     <p>What a world.</p>
     <img src="http://lorempixel.com/400/200/" alt="Lorem Pixel"/>
+    <br />
+    <Link to="/page-2">Link</Link>
   </div>
