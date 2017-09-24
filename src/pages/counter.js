9811252278ea34d2c4f893c24d2de1fb47baba55
Andric Tham <andric@nuisance.co>
2017-09-24 17:18:02 +0800

    Add counter page, which is a stateful component

---
+++ src/pages/counter.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class Counter extends Component {
+
+  render() {
+    return (
+      <div>
+        <h1>Counter</h1>
+        <p>Current count: 0</p>
+        <button>Plus</button>
+        <button>Minus</button>
+      </div>
+    );
+  }
+
+}
+
+export default Counter;
