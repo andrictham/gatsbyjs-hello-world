b1d1c3a5d229d124171df260b063bfad7e9326a3
Andric Tham <andric@nuisance.co>
2017-09-24 17:22:11 +0800

    Implement working counter

--- src/pages/counter.js
+++ src/pages/counter.js
@@ -2,13 +2,32 @@ import React, { Component } from 'react';
 
 class Counter extends Component {
 
+  constructor() {
+    super()
+    this.state = { count: 0 }
+    this.increment = this.increment.bind(this)
+    this.decrement = this.decrement.bind(this)
+  }
+
+  increment() {
+    this.setState({
+      count: this.state.count + 1
+    })
+  }
+
+  decrement() {
+    this.setState({
+      count: this.state.count - 1
+    })
+  }
+
   render() {
     return (
       <div>
         <h1>Counter</h1>
-        <p>Current count: 0</p>
-        <button>Plus</button>
-        <button>Minus</button>
+        <p>Current count: {this.state.count}</p>
+        <button onClick={this.increment}>Plus</button>
+        <button onClick={this.decrement}>Minus</button>
       </div>
     );
   }
