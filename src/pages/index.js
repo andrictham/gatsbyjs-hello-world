f17d6eb3ab63152af0ba8b371e1ede4f636421a4
Andric Tham <andric@nuisance.co>
2017-09-24 17:09:36 +0800

    Hello World

---
+++ src/pages/index.js
@@ -0,0 +1,8 @@
+import React from "react"
+
+export default () =>
+  <div style={{color: `tomato`}}>
+    <h1>Hello Gatsby!</h1>
+    <p>What a world.</p>
+    <img src="http://lorempixel.com/400/200/" alt="Lorem Pixel"/>
+  </div>
